733573272de032afb94812f99145b34e48268748
Divyansh Falodiya <divyanshfalodiyaab@gmail.com>
2020-02-18 19:54:08 +0530

    Added transitions to the navigation bar

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-var a;
+function start(){
+  document.querySelector(".initialImage").classList.toggle("title");
+  setTimeout(()=>{document.querySelector(".initialImage").classList.add("makeItSmall");},6000);
+  setTimeout(()=>{document.querySelector(".initialImage").style.cssText=" margin:0; width:80px;"},6000);
+}
