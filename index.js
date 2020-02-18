8f62ba1987c340fc7cbdb60cda7498db560e6cc7
Divyansh Falodiya <divyanshfalodiyaab@gmail.com>
2020-02-18 22:52:19 +0530

    Added the heading

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function start(){
   document.querySelector(".initialImage").classList.toggle("title");
   setTimeout(()=>{document.querySelector(".initialImage").classList.add("makeItSmall");},6000);
-  setTimeout(()=>{document.querySelector(".initialImage").style.cssText=" margin:0; width:80px;"},6000);
+  setTimeout(()=>{document.querySelector(".initialImage").style.cssText=" margin:0; width:80px;";},6000);
+  setTimeout(()=>{document.querySelector("#head").classList.add("changeHead");},6000);
+  setTimeout(()=>{document.querySelector("#head").style.cssText="font-size:4rem; transform:translate(0px,-150px);";},10000);
 }
