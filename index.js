50569fe99859d18772041b480c1eb7e2349ddc2d
DivyanshFalodiya <divyanshfalodiyaab@gmail.com>
2020-02-19 14:34:38 +0530

    Added a blue rotating circle in the background

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ function start(){
   setTimeout(()=>{document.querySelector(".initialImage").classList.add("makeItSmall");},6000);
   setTimeout(()=>{document.querySelector(".initialImage").style.cssText=" margin:0; width:80px;";},6000);
   setTimeout(()=>{document.querySelector("#head").classList.add("changeHead");},6000);
-  setTimeout(()=>{document.querySelector("#head").style.cssText="font-size:4rem; transform:translate(0px,-150px);";},10000);
+  setTimeout(()=>{document.querySelector("#head").style.cssText="font-size:4rem; transform:translate(-420px,-210px);";},10000);
+  setTimeout(()=>{document.querySelector(".smallcircle").classList.add("getCircle");},10000);
+  setTimeout(()=>{document.querySelector(".smallcircle").classList.add("rotate1");},13000);
+}
+function backCircle(){
 }
